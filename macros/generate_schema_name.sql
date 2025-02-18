d538ac53f07f4e4ec2d7d35f51679cbc747fcfdf
Tyler Rouze <tyler@tylerrouze.com>
2025-02-18 10:02:38 -0600

    Update generate_schema_name.sql

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,29 +1,27 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- set target_name = target.name -%}
-    {%- if target_name == 'prod' or target_name == 'uat' -%}
 
+    {%- if env_var('DBT_CLOUD_ENVIRONMENT_TYPE') == 'dev' -%}
         {%- if custom_schema_name is none -%}
 
             {{ default_schema }}
-            
-        {%- else -%}
 
-            {{ custom_schema_name | trim }}
-
-        {%- endif -%}
-
-    {%- else -%}
-        {%- if custom_schema_name is none -%}
-
-            {{ default_schema }}
-            
         {%- else -%}
 
             {{ default_schema }}_{{ custom_schema_name | trim }}
 
         {%- endif -%}
-    {%- endif -%}
 
+    {% else %}
+        {%- if custom_schema_name is none -%}
+
+            {{ default_schema }}
+
+        {%- else -%}
+
+            {{ custom_schema_name | trim }}
+
+        {%- endif -%}
+    {% endif %}
 {%- endmacro %}

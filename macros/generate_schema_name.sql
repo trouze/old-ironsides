29da9eeb3e2c1d5edc4eaa75b109e7763914245a
Tyler Rouze <tyler@tylerrouze.com>
2025-02-18 10:06:05 -0600

    Update generate_schema_name.sql

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,8 +1,19 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
+{% macro default__generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
 
-    {%- if env_var('DBT_CLOUD_ENVIRONMENT_TYPE') == 'dev' -%}
+    {%- if env_var('DBT_CLOUD_ENVIRONMENT_TYPE','empty') == 'empty' or env_var('DBT_CLOUD_ENVIRONMENT_TYPE') == 'staging' or env_var('DBT_CLOUD_ENVIRONMENT_TYPE') == 'prod' or var('schema_behavior') == 'deploy' -%}
+        {%- if custom_schema_name is none -%}
+
+            {{ default_schema }}
+
+        {%- else -%}
+
+            {{ custom_schema_name | trim }}
+
+        {%- endif -%}
+    
+    {%- elif env_var('DBT_CLOUD_ENVIRONMENT_TYPE') == 'dev' or target.name == 'dev' or target.name == 'ci' -%}
         {%- if custom_schema_name is none -%}
 
             {{ default_schema }}
@@ -12,7 +23,6 @@
             {{ default_schema }}_{{ custom_schema_name | trim }}
 
         {%- endif -%}
-
     {% else %}
         {%- if custom_schema_name is none -%}
 

3d81ac5a9b651d09fec996a55c55257e849dd608
Tyler Rouze <tyler.rouze@fishtownanalytics.com>
2025-04-22 21:14:08 +0000

    cleanup

--- macros/model_freshness.sql
+++
@@ -1,7 +0,0 @@
-{% test is_model_fresh(model, last_model_run_field, error_after, time_part) %}
-{{ config(store_failures = false, severity = 'error') }}
-    select
-    count(*)
-    from {{ model }}
-    having cast(max({{ last_model_run_field }}) as TIMESTAMP) < DATEADD({{ time_part }}, -cast({{ error_after }} as integer), current_timestamp())
-{% endtest %}
